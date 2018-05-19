97d347ee12a77aa663a49a8bf99501445097580f
Kaz Sto <kazuhisa@lazyarea.com>
2018-05-19 19:42:18 +0900

    update

--- provision/scripts/pkg.sh
+++ provision/scripts/pkg.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 yum update -y
-yum remove -y php php-*
 
-curl -O wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-yum localinstall -y remi-release-6.rpm 
+
+yum remove -y php php-*
+rm -fr/etc/php.* /etc/php
+
+
+yum -y install vim net-tools bind-utils ngrep
+
+
+curl -O -L http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+yum localinstall -y remi-release-7.rpm 
+
 
 cat > /etc/yum.repos.d/nginx.repo << NGINX
 
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/centos/7/$basearch/
+baseurl=http://nginx.org/packages/centos/7/\$basearch/
 gpgcheck=0
 enabled=1
 NGINX
 
 yum -y --enablerepo=nginx install nginx
 yum -y install mariadb mariadb-devel mariadb-server
-yum -y install --enablerepo=remi-php56 php php-fpm
+yum -y install --enablerepo=remi-php56 libmcrypt libmcrypt-devel php php-mcrypt php-fpm
 
+yum remove -y httpd 
 systemctl enable nginx
 systemctl enable mariadb-server
 
