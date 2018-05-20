31efd213148bb04d4eea56ceb1e5350b4ff12cf2
Kaz Sto <kazuhisa@lazyarea.com>
2018-05-21 00:16:22 +0900

    update

--- provision/scripts/pkg.sh
+++ provision/scripts/pkg.sh
@@ -24,7 +24,9 @@ NGINX
 
 yum -y --enablerepo=nginx install nginx
 yum -y install mariadb mariadb-devel mariadb-server
-yum -y install --enablerepo=remi-php56 libmcrypt libmcrypt-devel php php-mcrypt php-fpm composer
+yum -y install --enablerepo=remi-php56 libmcrypt libmcrypt-devel php php-mcrypt php-fpm composer \
+	php-pecl-xdebug php-devel php-mbstring php-pdo php-gd \
+        php-mcrypt php-zlib php-zip php-mysql  php-xml php-curl vim tree net-tools bind-utils ngrep nc
 
 yum remove -y httpd 
 systemctl enable nginx
