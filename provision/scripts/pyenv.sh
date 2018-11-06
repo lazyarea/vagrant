e6230a62797c31a73d4541aa6a70938c1ffd98b2
kazuhisa <hcz5cnorihrl93nvm8@gmail.com>
2018-11-07 01:38:24 +0900

    update

--- provision/scripts/pyenv.sh
+++ provision/scripts/pyenv.sh
@@ -2,7 +2,7 @@
 
 grep PYENV_ROOT ~vagrant/.bashrc > /dev/null 2>&1
 if [ $? -eq 1 ]; then
-
+  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 cat > ~vagrant/.bashrc << PYENV
 
 #-----------------
