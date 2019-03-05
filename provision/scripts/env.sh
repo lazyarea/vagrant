c98ef66a65f7b8d1985196ead8246c5612725f22
John Doe <johndoe@example.com>
2019-03-06 04:32:09 +0900

    update

--- provision/scripts/env.sh
+++ provision/scripts/env.sh
@@ -11,3 +11,14 @@ grep SELINUX=disabled /etc/selinux/config > /dev/null 2>&1
 if [ $? -eq 1 ]; then
   setenforce 0
 fi
+
+ls ~vagrant/.ssh/id_rsa
+if  [ $? -ne 0 ]; then
+cat >> ~vagrant/.bashrc << SSHADD
+
+#-----------------
+eval `ssh-agent`
+ssh-add $HOME/.ssh/id_rsa
+SSHADD
+
+fi
