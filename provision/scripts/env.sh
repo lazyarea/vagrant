1908be89165a03393d724da1fa101872134d3641
John Doe <johndoe@example.com>
2019-12-06 12:25:28 +0900

    update env

--- provision/scripts/env.sh
+++ provision/scripts/env.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-grep CAKEPHP_ENV > /dev/null 2>&1
-
-if [ $? -eq 1 ]; then
-  echo "export CAKEPHP_ENV=local" >> ~vagrant/.bashrc
-fi
 sed -ie 's/enforcing/disabled/g' /etc/selinux/config
 
 grep SELINUX=disabled /etc/selinux/config > /dev/null 2>&1
@@ -12,7 +7,7 @@ if [ $? -eq 1 ]; then
   setenforce 0
 fi
 
-ls ~vagrant/.ssh/id_rsa
+ls ~vagrant/.ssh/id_rsa > /dev/null 2>&1
 if  [ $? -ne 0 ]; then
 cat >> ~vagrant/.bashrc << SSHADD
 
