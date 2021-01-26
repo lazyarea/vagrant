8d6d0343630e65347f527721359de90bc8e9eaa1
lukaku3 <9835368+lukaku3@users.noreply.github.com>
2021-01-26 11:12:21 +0900

    Update pyenv.sh

--- provision/scripts/pyenv.sh
+++ provision/scripts/pyenv.sh
@@ -9,12 +9,12 @@ grep PYENV_ROOT ~vagrant/.bashrc > /dev/null 2>&1
 if [ $? -eq 1 ]; then
   git clone https://github.com/pyenv/pyenv.git ~vagrant/.pyenv
   sudo chown -R vagrant.  ~vagrant/.pyenv
-cat >> ~vagrant/.bashrc << PYENV
+cat >> ~vagrant/.bashrc << 'PYENV'
 
 #-----------------
-export PYENV_ROOT="\$HOME/.pyenv"
-export PATH="\$PYENV_ROOT/bin:\$PATH"
-eval "\$(pyenv init -)"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 PYENV
 
 fi
