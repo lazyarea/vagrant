85bf8be4445b3fcdfbd24304fa87eb7f0b26e9b3
lukaku3 <9835368+lukaku3@users.noreply.github.com>
2021-02-13 08:59:40 +0900

    Update nginx_conf.sh

--- provision/scripts/nginx_conf.sh
+++ provision/scripts/nginx_conf.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 yum install -y nginx
 cp /etc/nginx/conf.d/default.conf{,.org}
-cat > /etc/nginx/conf.d/default.conf << NGINX
+cat > /etc/nginx/conf.d/default.conf << 'NGINX'
 server {
     listen       *:80;
     server_name  localhost;
-
     #charset koi8-r;
     access_log  /var/log/nginx/xxxxx-access.log  main;
     error_log   /var/log/nginx/xxxxx-error.log;
-
     root   /var/www/xxxxx/webroot;
     index  index.php index.html index.htm;
-
     location / {
-        try_files \$uri \$uri?\$args \$uri/ /index.php?\$uri&\$args /index.php?\$args;
+        try_files $uri $uri?$args $uri/ /index.php?$uri&$args /index.php?$args;
     }
-
     error_page   500 502 503 504  /50x.html;
     location = /50x.html {
         root   /usr/share/nginx/html;
     }
-
-    location ~ \.php\$ {
-        try_files \$uri =404;
+    location ~ \.php$ {
+        try_files $uri =404;
         fastcgi_pass   unix:/var/run/php-fpm/php-fpm.sock;
         fastcgi_index  index.php;
-        fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
+        fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
         fastcgi_param  CAKEPHP_ENV 'local';
         fastcgi_read_timeout 120;
         include        fastcgi_params;
     }
-
     location = /favicon.ico {
         error_page    404 = @favicon;
         access_log    off;
         log_not_found off;
     }
-
     location @favicon {
         empty_gif;
         access_log    off;
@@ -46,4 +39,5 @@ server {
 }
 NGINX
 
+cp -p /etc/nginx/nginx.conf{,.org}
 sed -i 's/sendfile.*/sendfile off;/' /etc/nginx/nginx.conf
